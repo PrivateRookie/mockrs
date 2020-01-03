c7701de3b57b866b0c4731937e4eee81278f3049
PrivateRookie <996514515@qq.com>
2020-01-03 16:54:54 +0800

    feature: add generate fake data function
    now user can generate fake base tera template.
    
    changes:
      now "serve" and "gen" subcommands are use to serve json file
      and generate fake data json file speratally

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
 use actix_web::{middleware, web, App, HttpServer};
 use dotenv::dotenv;
+use jen::generator::Generator;
 use structopt::StructOpt;
+use std::io::{Error, ErrorKind, prelude::* };
 
 mod api;
 mod db;
 
 #[derive(StructOpt, Debug, Clone)]
 #[structopt(
-    name = "mockrs",
-    version = "0.1.0",
+    name = env!("CARGO_PKG_NAME"),
+    version = env!("CARGO_PKG_VERSION"),
     author = "PrivateRookie <996514515@qq.com>",
-    about = "a mock restful json http server"
+    about = env!("CARGO_PKG_DESCRIPTION")
 )]
-struct Config {
-    /// json file as database
-    #[structopt(required = true, env = "MOCKRS_DB_FILE")]
-    db_file: String,
+enum Config {
+    /// Run http json server
+    Serve {
+        /// Json file as database
+        #[structopt(required = true, env = "MOCKRS_DB_FILE")]
+        db_file: String,
 
-    /// Listen ip
-    #[structopt(long, default_value = "127.0.0.1", env = "MOCKRS_HOST")]
-    host: String,
+        /// Listen ip
+        #[structopt(long, default_value = "127.0.0.1", env = "MOCKRS_HOST")]
+        host: String,
 
-    /// Listen port
-    #[structopt(long, default_value = "9000", env = "MOCKRS_PORT")]
-    port: usize,
+        /// Listen port
+        #[structopt(long, default_value = "9000", env = "MOCKRS_PORT")]
+        port: usize,
+    },
+
+    /// Generate fake data based on template
+    Gen {
+        /// Template file to generate json file
+        #[structopt(required = true)]
+        template: String,
+
+        /// Output json file
+        #[structopt(long)]
+        output: Option<String>,
+    },
 }
 
 #[actix_rt::main]
@@ -31,24 +47,46 @@ async fn main() -> std::io::Result<()> {
     dotenv().ok();
     pretty_env_logger::init();
 
-    let conf: Config = Config::from_args();
-    let db = db::Database::load(&conf.db_file);
-    let web_db = web::Data::new(db);
-    HttpServer::new(move || {
-        App::new()
-            .register_data(web_db.clone())
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/index").route(web::get().to(api::server_info)))
-            .service(web::scope("/_actions").route("/flush", web::post().to(api::flush)))
-            .service(
-                web::resource("/*")
-                    .route(web::get().to(api::do_get))
-                    .route(web::post().to(api::do_post))
-                    .route(web::put().to(api::do_post))
-                    .route(web::delete().to(api::do_delete)),
-            )
-    })
-    .bind(format!("{}:{}", conf.host, conf.port))?
-    .start()
-    .await
+    match Config::from_args() {
+        Config::Serve {
+            db_file,
+            host,
+            port,
+        } => {
+            let db = db::Database::load(&db_file);
+            let web_db = web::Data::new(db);
+            HttpServer::new(move || {
+                App::new()
+                    .register_data(web_db.clone())
+                    .wrap(middleware::Logger::default())
+                    .service(web::resource("/index").route(web::get().to(api::server_info)))
+                    .service(web::scope("/_actions").route("/flush", web::post().to(api::flush)))
+                    .service(
+                        web::resource("/*")
+                            .route(web::get().to(api::do_get))
+                            .route(web::post().to(api::do_post))
+                            .route(web::put().to(api::do_post))
+                            .route(web::delete().to(api::do_delete)),
+                    )
+            })
+            .bind(format!("{}:{}", host, port))?
+            .start()
+            .await
+        }
+        Config::Gen { template, output } => {
+            match Generator::new(template) {
+                Err(_) => Err(Error::new(ErrorKind::NotFound, "can not find template")),
+                Ok(mut gen) => {
+                    match output {
+                        None => println!("{}", gen.create()),
+                        Some(output) => {
+                            let mut f = std::fs::File::create(output)?;
+                            f.write(gen.create().as_bytes())?;
+                        }
+                    }
+                    Ok(())
+                }
+            }
+        }
+    }
 }
